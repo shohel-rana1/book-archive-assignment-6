0e9a39debd4747564522a4919bc1ce4e59c8f330
shohel-rana1 <gmshohel53@gmail.com>
2021-09-02 20:09:18 +0600

    update url

--- book.js
+++ book.js
@@ -10,7 +10,7 @@ const searchBooks = () => {
         // clear input data 
         searchFiled.value = '';
         // load data 
-        const url = `http://openlibrary.org/search.json?q=${searchText}`
+        const url = `https://openlibrary.org/search.json?q=${searchText}`
         fetch(url)
             .then(res => res.json())
             .then(data => displayBooks(data))
